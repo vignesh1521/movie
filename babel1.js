de3169ddb5e794eb390985cc183a4fdef536c1fa
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2022-07-19 15:39:28 +0530

    Add files via upload

---
+++ babel1.js
@@ -0,0 +1,74 @@
+const First = (props) =>{
+  return(
+
+
+<li className='con'>
+
+<div className='container'>
+<div className='avatar'>
+  <img src={props.src}></img>
+  <div className='details'>
+<p>
+  {props.title}
+
+</p>
+
+<div className='hd'>
+
+	<a  href={props.href}>Download</a>
+
+</div>
+</div>
+</div>
+</div>
+
+</li>
+  )
+  
+}
+
+const App = () => {
+  return(
+     <span>
+         <First src='img/maamanithan.jpg'
+          title='	maamanithan (2022) tamil movies  HD 720 watch online'  
+          href='http://vidmx.xyz/yembed-og4omgms0bnh.html'
+ 
+           />
+           <First src='img/bestie.jpg'
+          title=' bestie (2022) tamil movies  HD 720 watch online'  
+          href='http://vidmx.xyz/embed-3wskdd68fzxx.html'
+ 
+           />
+           <First src='img/veetlavishesham.jpg'
+          title=' veetla vishesham (2022) tamil movies  HD 720 watch online'  
+          href='http://vidmx.xyz/embed-kf186sgcb7cy.html'
+ 
+           />
+     <First src='img/vikram.jpeg'
+          title=' vikram (2022) tamil movies  HD 720 watch online'  
+          href='http://vidmx.xyz/embed-62g253t7zk39.html'
+
+           />
+            <First src='img/don.jpeg'
+          title=' don (2022) tamil movies  HD 720 watch online'  
+          href='http://vidmx.xyz/embed-ej8zf2si0i95.html'
+         
+           />
+
+           {<script type="text/javascript">
+  atOptions = {
+    'key' : 'e9bc2b3b6af8f4f8ae1deaf309c66d0d',
+    'format' : 'iframe',
+    'height' : 60,
+    'width' : 468,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/e9bc2b3b6af8f4f8ae1deaf309c66d0d/invoke.js"></scr' + 'ipt>');
+</script>}
+       
+    </span>
+  )
+      }
+
+
