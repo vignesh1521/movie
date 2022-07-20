ea17f7216f09a07881f809373335ba3be9daaab2
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2022-07-20 20:55:48 +0530

    Add files via upload

---
+++ series/babel1.js
@@ -0,0 +1,40 @@
+const First = (props) =>{
+  return(
+
+
+<li className='con'>
+
+<div className='container'>
+<div className='avatar'>
+  <img src={props.src}></img>
+  <div className='details'>
+<p>
+  {props.title}
+
+</p>
+
+<div className='hd'>
+
+	<a  href={props.href}>Download</a>
+
+</div>
+</div>
+</div>
+</div>
+
+</li>
+  )
+  
+}
+
+
+const App = () => {
+  return(
+     <span>
+       
+
+    </span>
+  )
+      }
+
+
